bd63d30d5ca2550ab50c60d8e9b3caa897ed7f61
fanqiliang <fanqiliang12@gmail.com>
2013-09-24 09:47:56 +0800

    add some code,but this is not the last copyright

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -1,14 +1,21 @@
 package lexer;
 
+import java.io.IOException;
 import java.io.InputStream;
+
+import com.sun.org.apache.regexp.internal.recompile;
 
 import util.Todo;
 
 import lexer.Token.Kind;
 
+@SuppressWarnings("unused")
 public class Lexer {
 	String fname; // the input file name to be compiled
 	InputStream fstream; // input stream for the above file
+	Token token = new Token();
+	boolean b = true;
+	int index = 0;
 
 	public Lexer(String fname, InputStream fstream) {
 		this.fname = fname;
@@ -24,18 +31,205 @@ public class Lexer {
 			// The value for "lineNum" is now "null",
 			// you should modify this to an appropriate
 			// line number for the "EOF" token.
-			return new Token(Kind.TOKEN_EOF, null);
+			return new Token(Kind.TOKEN_EOF, token.lineNum);
 
 		// skip all kinds of "blanks"
 		while (' ' == c || '\t' == c || '\n' == c) {
 			c = this.fstream.read();
 		}
 		if (-1 == c)
-			return new Token(Kind.TOKEN_EOF, null);
+			return new Token(Kind.TOKEN_EOF, token.lineNum++);
 
 		switch (c) {
 		case '+':
-			return new Token(Kind.TOKEN_ADD, null);
+			return new Token(Kind.TOKEN_ADD, token.lineNum);
+		case '&':
+			c = this.fstream.read();
+			switch (c) {
+			case '&':
+				return new Token(Kind.TOKEN_AND, token.lineNum);
+			default:
+				new Todo();
+				return null;
+			}
+		case '=':
+			return new Token(Kind.TOKEN_ASSIGN, token.lineNum);
+		case 'b':
+			String str = "oolean";
+
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_BOOLEAN, token.lineNum);
+			}
+			b = true;
+		case 'c':
+			str = "lass";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_CLASS, token.lineNum);
+			}
+			b = true;
+		case ',':
+			return new Token(Kind.TOKEN_COMMER, token.lineNum);
+		case '.':
+			return new Token(Kind.TOKEN_DOT, token.lineNum);
+		case 'e':
+			str = "lse";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_ELSE, token.lineNum);
+			}
+			b = true;
+			str = "xtends";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_EXTENDS, token.lineNum);
+			}
+			b = true;
+			
+	//	case 'EOF'
+			
+		case 'f':
+			str = "alse";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_FALSE, token.lineNum);
+			}
+			b = true;
+			
+	//	case 'IN'
+
+		case 'i':
+			str = "f";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_IF, token.lineNum);
+			}
+			b = true;
+	/* zheli*/		str = "xtends";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_EXTENDS, token.lineNum);
+			}
+			b = true;
+		case '{':
+			return new Token(Kind.TOKEN_LBRACE, token.lineNum);
+		case '[':
+			return new Token(Kind.TOKEN_LBRACK, token.lineNum);
+		case 'l':
+			str = "ength";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_LENGTH, token.lineNum);
+			}
+			b = true;
+		case '(':
+			return new Token(Kind.TOKEN_LPAREN, token.lineNum);
+		case '<':
+			return new Token(Kind.TOKEN_LT, token.lineNum);
+		case 'm':
+			str = "ain";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_MAIN, token.lineNum);
+			}
+			b = true;
+		case 'n':
+			str = "ew";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_NEW, token.lineNum);
+			}
+			b = true;
+		case '!':
+			return new Token(Kind.TOKEN_NOT, token.lineNum);
+		case 'o':
+			str = "ut";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_OUT, token.lineNum);
+			}
+			b = true;
+		
+		case 'p':
+			str = "rintln";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_PRINTLN, token.lineNum);
+			}
+			b = true;
+			str = "ublic";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_PUBLIC, token.lineNum);
+			}
+			b = true;
+		case '}':
+			return new Token(Kind.TOKEN_RBRACE, token.lineNum);	
+		case ']':
+			return new Token(Kind.TOKEN_RBRACK, token.lineNum);
+		case 'r':
+			str = "eturn";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_RETURN, token.lineNum);
+			}
+			b = true;
+		case ')':
+			return new Token(Kind.TOKEN_RPAREN, token.lineNum);
+		case ';':
+			return new Token(Kind.TOKEN_SEMI, token.lineNum);
+		case 's':
+			str = "tatic";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_STATIC, token.lineNum);
+			}
+			b = true;
+		case 'S':
+			str = "tring";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_STRING, token.lineNum);
+			}
+			b = true;
+			str = "ystem";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_SYSTEM, token.lineNum);
+			}
+			b = true;
+		case '-':
+			return new Token(Kind.TOKEN_SUB, token.lineNum);
+		case 't':
+			str = "tatic";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_THIS, token.lineNum);
+			}
+			b = true;
+			str = "rue";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_TRUE, token.lineNum);
+			}
+			b = true;
+		case '*':
+			return new Token(Kind.TOKEN_TIMES, token.lineNum);
+		case 'v':
+			str = "oid";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_VOID, token.lineNum);
+			}
+			b = true;
+		case 'w':
+			str = "hile";
+			b = Comparing(str, c);
+			if (b) {
+				return new Token(Kind.TOKEN_WHILE, token.lineNum);
+			}
+			b = true;
 		default:
 			// Lab 1, exercise 2: supply missing code to
 			// lex other kinds of tokens.
@@ -47,6 +241,22 @@ public class Lexer {
 			new Todo();
 			return null;
 		}
+	}
+	
+	public boolean Comparing(String str, int c) {
+		while (b && index < str.length()) {
+			try {
+				c = this.fstream.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (c != str.charAt(index)) {
+				b = false;
+				break;
+			}				
+		}
+		return b;
 	}
 
 	public Token nextToken() {
